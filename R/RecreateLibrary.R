f7b5ed84d49aff282011ea593ed8ce67f1b9717e
Jason Fisher <jfisher@usgs.gov>
2017-07-10 13:59:14 -0700

    Tidy doc and example

--- R/RecreateLibrary.R
+++ R/RecreateLibrary.R
@@ -4,6 +4,8 @@
 #'
 #' @param file 'character'.
 #'   Name of the file for writing (or reading) the list of package names.
+#'   If the name does not contain an absolute path,
+#'   the file name is relative to the current working directory, see \code{\link{getwd}()} command.
 #' @param lib 'character'.
 #'   The library tree(s) to search through when locating installed packages (see \code{\link{.libPaths}})
 #'   or the library directory where to install packages.
@@ -16,40 +18,42 @@
 #' to print to a text file a complete list of names for packages located under your current \R library tree(s).
 #' Uninstall the older version of \R if it is no longer needed.
 #' On a freshly installed version of \R, with the \pkg{inlmisc} package available,
-#' run the \code{RecreateLibrary()} command to download and install the packages listed in the package-list text file.
+#' run the \code{RecreateLibrary()} command to download and install
+#' the packages listed in the package-list text file.
 #'
 #' @author J.C. Fisher, U.S. Geological Survey, Idaho Water Science Center
 #'
-#' @seealso \code{\link[utils]{update.packages}}, \code{\link[utils]{install.packages}}
+#' @seealso \code{\link[utils]{installed.packages}}, \code{\link[utils]{install.packages}}
 #'
 #' @keywords utilities
 #'
 #' @examples
 #' # Run on old version of R
-#' SavePackageNames()
+#' SavePackageNames("package-list.txt")
 #'
 #' \dontrun{
-#' # Run on new version of R (assumes working directory has not changed)
-#' RecreateLibrary(repos = c(CRAN = "https://cloud.r-project.org/",
-#'                           GRAN = "https://owi.usgs.gov/R"))
+#' # Run on new version of R, and ensure inlmisc package is available.
+#' repos <- c(CRAN = "https://cloud.r-project.org/", GRAN = "https://owi.usgs.gov/R")
+#' if (!"inlmisc" %in% rownames(utils::installed.packages()))
+#'   utils::install.packages("inlmisc", repos = repos["CRAN"])
+#' inlmisc::RecreateLibrary("package-list.txt", repos = repos)
 #' }
-#'
-#' unlink("packagelist.txt")
 #'
 #' @rdname RecreateLibrary
 #' @export
 
-SavePackageNames <- function(file="packagelist.txt", lib=NULL) {
+SavePackageNames <- function(file="package-list.txt", lib=NULL) {
 
   if (is.null(lib)) lib <- .libPaths()
 
-  pkgs <- utils::installed.packages(lib, noCache=TRUE)[, 1, drop=FALSE]
+  pkgs <- utils::installed.packages(lib, noCache=TRUE)[, 1]
+  pkgs <- sort(unique(pkgs))
   meta <- c(sprintf("# Date modified: %s UTC", format(Sys.time(), tz="GMT")),
             sprintf("# Running under: %s", utils::sessionInfo()$running),
             with(R.version, sprintf("# R version: %s.%s (%s-%s-%s)", major, minor, year, month, day)),
             sprintf("# Platform: %s", R.version$platform),
             sprintf("# User: %s", Sys.info()["user"]))
-  m <- rbind(matrix(meta), sort(unique(pkgs)))
+  m <- matrix(c(meta, pkgs), ncol=1)
   utils::write.table(m, file, quote=FALSE, row.names=FALSE, col.names=FALSE)
   cat(sprintf("Package list written to: \"%s\"\n", normalizePath(path.expand(file))))
 
@@ -59,17 +63,20 @@ SavePackageNames <- function(file="packagelist.txt", lib=NULL) {
 #' @rdname RecreateLibrary
 #' @export
 
-RecreateLibrary <- function(file="packagelist.txt", lib=NULL, repos=getOption("repos")) {
+RecreateLibrary <- function(file="package-list.txt", lib=NULL, repos=getOption("repos")) {
 
   if (is.null(lib)) lib <- .libPaths()[1]
 
-  if(!file.exists(file)) stop("package-list file not found")
+  if (!file.exists(file)) {
+    msg <- sprintf("Can't find package-list file: ", normalizePath(path.expand(file)))
+    stop(msg)
+  }
   type <- ifelse(Sys.info()["sysname"] == "Windows", "win.binary", "source")
   utils::update.packages(ask=FALSE, repos=repos, type=type)
   pkgs <- unique(utils::read.table(file, colClasses="character", flush=TRUE)[, 1])
   installed_pkgs <- utils::installed.packages()[, "Package"]
   pkgs <- pkgs[!pkgs %in% installed_pkgs]
-  if (length(pkgs) == 0) return()
+  if (length(pkgs) == 0) return(invisible(NULL))
   contriburl <- utils::contrib.url(repos=repos, type=getOption("pkgType"))
   available_pkgs <- utils::available.packages(contriburl, type=type)
   is <- pkgs %in% available_pkgs
@@ -78,9 +85,8 @@ RecreateLibrary <- function(file="packagelist.txt", lib=NULL, repos=getOption("r
     warning(sprintf(fmt, paste(pkgs[!is], collapse=", ")))
   }
   pkgs <- pkgs[is]
-  if (length(pkgs) == 0) return()
+  if (length(pkgs) == 0) return(invisible(NULL))
   utils::install.packages(pkgs, lib[1], repos=repos, type=type)
 
   invisible(NULL)
 }
-
