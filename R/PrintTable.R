a6415bfc6ea933a0418235bd1faa3bd6441f4070
Jason Fisher <jfisher@usgs.gov>
2017-11-30 15:38:37 -0800

    Refine code in PrintTable

--- R/PrintTable.R
+++ R/PrintTable.R
@@ -8,6 +8,8 @@
 #'   Something.
 #' @param align 'character'.
 #'   Something.
+#' @param digits 'integer'.
+#'   Something.
 #' @param label 'character'.
 #'   Something.
 #' @param title 'character'.
@@ -18,10 +20,15 @@
 #'   Something.
 #' @param nrec 'integer'.
 #'   Something.
+#' @param hline 'integer'.
+#'   Something.
 #' @param na 'character'.
 #'   Something.
-#' @param rm_dup 'character' or 'integer'.
+#' @param rm_dup 'integer'.
 #'   Something.
+#' @param landscape 'logical'.
+#'   Something.
+#'   This option requires \code{\\usepackage[pdftex]{lscape}} in the LaTeX preamble.
 #'
 #' @details
 #'   Requires \code{\\usepackage{caption}}, \code{\\usepackage{booktabs}},
@@ -50,25 +57,28 @@
 #'            nrec = c(40L, 45L), rm_dup = 1L)
 #'
 
-PrintTable <- function(d, colheadings=NULL, align=NULL, label=NULL,
+PrintTable <- function(d, colheadings=NULL, align=NULL, digits=NULL, label=NULL,
                        title=NULL, headnotes=NULL, footnotes=NULL,
-                       nrec=nrow(d), na="--", rm_dup=NULL) {
+                       nrec=nrow(d), hline=NULL, na="--",
+                       rm_dup=NULL, landscape=FALSE) {
 
   checkmate::assertDataFrame(d, min.rows=1, min.cols=1)
   checkmate::assertCharacter(colheadings, any.missing=FALSE, len=ncol(d), null.ok=TRUE)
   checkmate::assertCharacter(align, any.missing=FALSE, len=ncol(d), null.ok=TRUE)
+  checkmate::assertIntegerish(digits, any.missing=FALSE, len=ncol(d), null.ok=TRUE)
   checkmate::assertString(label, null.ok=TRUE)
   checkmate::assertString(title, null.ok=TRUE)
   checkmate::assertString(headnotes, null.ok=TRUE)
   checkmate::assertString(footnotes, null.ok=TRUE)
   checkmate::assertIntegerish(nrec, lower=1, any.missing=FALSE, min.len=1, max.len=2)
+  checkmate::assertIntegerish(hline, lower=1, upper=nrow(d) - 1L, any.missing=FALSE, null.ok=TRUE)
   checkmate::assertString(na, null.ok=TRUE)
-  checkmate::qassert(rm_dup, c("0", "S+", sprintf("X+(0,%d)", ncol(d))))
+  checkmate::assertInt(rm_dup, lower=1, upper=ncol(d), null.ok=TRUE)
+  checkmate::assertFlag(landscape)
 
   if (!is.null(colheadings))
     colnames(d) <- sprintf("{\\normalfont\\bfseries\\sffamily \\makecell{%s}}",
                            colheadings)
-  align <- c("r", align)
 
   cap1 <- strwrap(title, width=.Machine$integer.max)
   cap2 <- strwrap(headnotes, width=.Machine$integer.max)
@@ -78,13 +88,18 @@ PrintTable <- function(d, colheadings=NULL, align=NULL, label=NULL,
 
   n <- nrow(d)
   if (n > nrec[1]) {
-    if (is.na(nrec[2])) nrec[2] <- nrec[1]
+    if (length(nrec) == 1) nrec[2] <- nrec[1]
     n <- unique(c(cumsum(c(nrec[1], rep(nrec[2], (n - nrec[1]) %/% nrec[2]))), n))
+  }
+
+  if (landscape) {
+    cat("\\begin{landscape}\n")
+    on.exit(cat("\\end{landscape}\n"))
   }
 
   for (i in seq_along(n)) {
     if (i == 2) cat("\\captionsetup[table]{list=no}\n")
-    if (i == 1) {
+    if (i == 1){
       idxs <- 1:n[i]
       caption <- c(sprintf("%s\\par \\medskip [\\footnotesize{%s}]", cap1, cap2), cap1)
     } else {
@@ -93,14 +108,28 @@ PrintTable <- function(d, colheadings=NULL, align=NULL, label=NULL,
       label <- NULL
       cat("\\addtocounter{table}{-1}\n")
     }
-    hline.after <- c(-1, 0, if (i == length(n)) length(idxs) else NULL)
 
     tbl <- d[idxs, ]
-    for (id in rm_dup) tbl[[id]][duplicated(tbl[[id]])] <- ""
+    if (!is.null(rm_dup))
+      for (j in rev(seq_len(rm_dup)))
+        tbl[[j]][duplicated(tbl[, seq_len(j), drop=FALSE])] <- ""
+
     tbl <- xtable::xtable(tbl)
     xtable::caption(tbl) <- caption
-    xtable::label(tbl) <- label
-    xtable::align(tbl) <- align
+    if (length(caption) > 0) xtable::label(tbl) <- label
+    if (!is.null(align)) xtable::align(tbl) <- c("l", align)
+    if (!is.null(digits)) xtable::digits(tbl) <- c(0L, digits)
+    add.to.row <- NULL
+
+    hline.after <- sort(unique(stats::na.omit(c(-1, 0, match(c(hline, nrow(d)), idxs)))))
+
+    if (!is.null(footnotes) && i == length(n)) {
+      fmt <- "\\midrule\n\\multicolumn{%s}{l}{\\footnotesize{%s}}\\\\"
+      cmd <- sprintf(fmt, ncol(tbl), footnotes)
+      add.to.row <- list(pos=list(nrow(tbl)), command=cmd)
+      hline.after <- head(hline.after, -1)
+    }
+
     print(tbl,
           include.rownames=FALSE,
           caption.placement="top",
@@ -108,6 +137,7 @@ PrintTable <- function(d, colheadings=NULL, align=NULL, label=NULL,
           sanitize.colnames.function=function(x){x},
           size="\\small",
           sanitize.text.function=identity,
+          add.to.row=add.to.row,
           hline.after=hline.after,
           NA.string=na)
 
