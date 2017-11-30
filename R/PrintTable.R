c6ddd3da093fe24ec572a31955873c5413c22011
Jason Fisher <jfisher@usgs.gov>
2017-11-29 22:26:37 -0800

    Minor changes

--- R/PrintTable.R
+++ R/PrintTable.R
@@ -1,6 +1,6 @@
 #' Print as LaTeX Table
 #'
-#' Something...
+#' This function prints the LaTeX code associated with the supplied data frame.
 #'
 #' @param d 'data.frame'.
 #'   Something.
@@ -20,6 +20,8 @@
 #'   Something.
 #' @param na 'character'.
 #'   Something.
+#' @param rm_dup 'character' or 'integer'.
+#'   Something.
 #'
 #' @details
 #'   Requires \code{\\usepackage{caption}}, \code{\\usepackage{booktabs}},
@@ -29,7 +31,7 @@
 #'
 #' @seealso \code{\link[xtable]{xtable}}
 #'
-#' @keywords IO
+#' @keywords print
 #'
 #' @export
 #'
@@ -37,22 +39,31 @@
 #' d <- datasets::iris
 #' d <- d[, c("Species", "Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")]
 #' colheadings <- c("Species of \\\\ Iris",
-#'                  "Sepal \\\\ length \\\\ (cm)",
-#'                  "Sepal \\\\ width \\\\ (cm)",
-#'                  "Petal \\\\ length \\\\ (cm)",
-#'                  "Petal \\\\ width \\\\ (cm)")
+#'                  "Sepal \\\\ length \\\\ (cm)", "Sepal \\\\ width \\\\ (cm)",
+#'                  "Petal \\\\ length \\\\ (cm)", "Petal \\\\ width \\\\ (cm)")
 #' align <- c("l", "r", "r", "r", "r")
 #' title <- "Measurments of sepal length and width and petal length and width,
 #'           for three species of Iris flower."
 #' headnotes <- "\\textbf{Species of Iris}: inlcudes setosa, versicolor, and virginica.
 #'               \\textbf{Abbreviations}: cm, centimeters"
 #' PrintTable(d, colheadings, align, title = title, headnotes = headnotes,
-#'            nrec = c(40L, 45L))
+#'            nrec = c(40L, 45L), rm_dup = 1L)
 #'
 
 PrintTable <- function(d, colheadings=NULL, align=NULL, label=NULL,
                        title=NULL, headnotes=NULL, footnotes=NULL,
-                       nrec=nrow(d), na="--") {
+                       nrec=nrow(d), na="--", rm_dup=NULL) {
+
+  checkmate::assertDataFrame(d, min.rows=1, min.cols=1)
+  checkmate::assertCharacter(colheadings, any.missing=FALSE, len=ncol(d), null.ok=TRUE)
+  checkmate::assertCharacter(align, any.missing=FALSE, len=ncol(d), null.ok=TRUE)
+  checkmate::assertString(label, null.ok=TRUE)
+  checkmate::assertString(title, null.ok=TRUE)
+  checkmate::assertString(headnotes, null.ok=TRUE)
+  checkmate::assertString(footnotes, null.ok=TRUE)
+  checkmate::assertIntegerish(nrec, lower=1, any.missing=FALSE, min.len=1, max.len=2)
+  checkmate::assertString(na, null.ok=TRUE)
+  checkmate::qassert(rm_dup, c("0", "S+", sprintf("X+(0,%d)", ncol(d))))
 
   if (!is.null(colheadings))
     colnames(d) <- sprintf("{\\normalfont\\bfseries\\sffamily \\makecell{%s}}",
@@ -61,6 +72,9 @@ PrintTable <- function(d, colheadings=NULL, align=NULL, label=NULL,
 
   cap1 <- strwrap(title, width=.Machine$integer.max)
   cap2 <- strwrap(headnotes, width=.Machine$integer.max)
+
+  idxs <- which(unlist(lapply(d, is.factor)))
+  d[, idxs] <- lapply(d[, idxs, drop=FALSE], as.character)
 
   n <- nrow(d)
   if (n > nrec[1]) {
@@ -81,7 +95,9 @@ PrintTable <- function(d, colheadings=NULL, align=NULL, label=NULL,
     }
     hline.after <- c(-1, 0, if (i == length(n)) length(idxs) else NULL)
 
-    tbl <- xtable::xtable(d[idxs, ])
+    tbl <- d[idxs, ]
+    for (id in rm_dup) tbl[[id]][duplicated(tbl[[id]])] <- ""
+    tbl <- xtable::xtable(tbl)
     xtable::caption(tbl) <- caption
     xtable::label(tbl) <- label
     xtable::align(tbl) <- align
