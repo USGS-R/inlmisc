1afee5a05d578b32dbe910f93e279bbecd5f8b53
Jason Fisher <jfisher@usgs.gov>
2018-04-17 12:49:16 -0700

    Add columns to PrintFigure

--- R/PrintFigure.R
+++ R/PrintFigure.R
@@ -10,8 +10,8 @@
 #'   Vector of figure plotting commands written in \R.
 #'   The length of the vector is either equal to the number of subfigures, or 1 when a single plot is desired.
 #'   An element in the vector contains the commands for creating a single plot.
-#' @param n 'integer'.
-#'   Maximum number of subfigures to place on a page.
+#' @param nr,nc 'integer'.
+#'   Maximum number of rows and columns in the subfigure layout on a page.
 #' @param label 'character'.
 #'   String containing the LaTeX label anchor.
 #'   Subfigures are labeled using a concatenation of the \code{label} argument and a letter.
@@ -38,18 +38,21 @@
 #' @export
 #'
 #' @examples
-#' fig <- sprintf("par(mar = c(2.1, 2.1, 0, 0.1)); plot(runif(%s))", 1:10)
-#' headings <- sprintf("Example heading, n = %s", 1:10)
-#' title <- "Example title"
-#' PrintFigure(fig, 2, "id", title, headings, height = 2)
+#' fig <- sprintf("par(mar = c(2.1, 2.1, 0.1, 1.1)); plot(runif(%s))", 10)
+#' title <- "Example of a figure caption."
+#' PrintFigure(fig, label = "id", title = title)
+#'
+#' fig <- sprintf("par(mar = c(2.1, 2.1, 0.1, 1.1)); plot(runif(%s))", 1:10)
+#' headings <- sprintf("Example subfigure caption, n=%s", 1:10)
+#' PrintFigure(fig, 3, 2, "id", title, headings, width = 3, height = 2)
 #'
 #' \dontrun{
 #' sink("figure-example.Rnw")
 #' cat("\\documentclass{article}",
-#'     "\\usepackage[labelsep=period,labelfont=bf]{caption}",
+#'     "\\usepackage[labelsep=period, labelfont=bf]{caption}",
 #'     "\\usepackage{subcaption}",
 #'     "\\begin{document}", sep = "\n")
-#' PrintFigure(fig, 4, "id", title, headings, height = 2)
+#' PrintFigure(fig, 3, 2, "id", title, headings, width = 3, height = 2)
 #' cat("\\end{document}\n")
 #' sink()
 #' knitr::knit2pdf("figure-example.Rnw", clean = TRUE)  # requires TeX installation
@@ -59,26 +62,31 @@
 #' }
 #'
 
-PrintFigure <- function(fig, n=length(fig), label="", title="", headings="", width=7, height=7) {
+PrintFigure <- function(fig, nr=1, nc=1, label="", title="", headings="", width=7, height=7) {
 
   # check arguments
   checkmate::assertCharacter(fig, any.missing=FALSE, min.len=1)
-  checkmate::assertInt(n, lower=1)
+  checkmate::assertInt(nr, lower=1)
+  checkmate::assertInt(nc, lower=1)
   checkmate::assertString(label)
   checkmate::assertString(title)
   checkmate::assertCharacter(headings, any.missing=FALSE, min.len=1, max.len=length(fig))
   checkmate::assertNumber(width, finite=TRUE)
   checkmate::assertNumber(height, finite=TRUE)
 
-  # calculate number of plots
+  # maximum number of plots on a page
+  n <- nr * nc
+
+  # total number of plots on all pages
   np <- length(fig)
 
-  # recycle headings
+  # recycle subfigure captions
   headings <- rep(headings, length.out=np)
 
   cat("\n")
   for (i in seq_len(np)) {
     sublabel <- if (np > 1) sprintf("%s_%s", label, letters[i]) else label
+
     if (i == n) {
       caption <- title
     } else if (i > n && ((i %% n) == 0 || i == np)) {
@@ -86,21 +94,28 @@ PrintFigure <- function(fig, n=length(fig), label="", title="", headings="", wid
     } else {
       caption <- NA
     }
+
     if (i == n + 1L) cat("\\captionsetup[figure]{list=no}\n\n")
+
     if ((i - 1L) %% n == 0) {
       cat("\\begin{figure}\n")
       if (i > 1) cat("  \\ContinuedFloat\n")
-    } else {
+    } else if ((i - 1L) %% nc == 0) {
       cat("  \\par\\bigskip\n")
+    } else {
+      cat("  \\qquad\n")
     }
+
     if (np > 1) {
-      cat("  \\begin{subfigure}{\\textwidth}\n")
+      cat(sprintf("  \\begin{subfigure}{%.2f\\textwidth}\n", 1 / nc))
       cat(sprintf("    \\caption{{%s \\label{fig:%s}}}\n", headings[i], sublabel))
     }
-    cat(sprintf("    <<%s, echo=FALSE, results='asis', fig.width=%s, fig.height=%s>>=\n", sublabel, width, height))
-    cat(sprintf("    %s\n", fig[[i]]))
+    cat(sprintf("    <<%s, echo=FALSE, results='asis', fig.width=%s, fig.height=%s>>=\n",
+                sublabel, width, height))
+    cat(sprintf("    %s\n", fig[i]))
     cat("    @\n")
     if (np > 1) cat("  \\end{subfigure}\n")
+
     if (!is.na(caption)) cat(sprintf("  \\caption{{%s}}\n", caption))
     if (i %% n == 0 || i == np) cat("\\end{figure}\n\n")
     if (i > n && i == np) cat("\\captionsetup[figure]{list=yes}\n\n")
