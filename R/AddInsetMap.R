37af6ca3f1c7811d0a673e1f74258249d554d4d0
Jason Fisher <jfisher@usgs.gov>
2018-05-18 19:59:44 -0700

    Minor changes

--- R/AddInsetMap.R
+++ R/AddInsetMap.R
@@ -37,11 +37,12 @@
 #' @export
 #'
 #' @examples
-#' id <- rgdal::readOGR(system.file("extdata/id.geojson", package = "inlmisc")[1])
+#' file <- system.file("extdata/county.geojson", package = "inlmisc")[1]
+#' county <- rgdal::readOGR(file)
 #' ext <- c(-113.4005, -112.2764, 43.30, 44.11)
-#' PlotMap(id, xlim = ext[1:2], ylim = ext[3:4], dms.tick = TRUE)
-#' sp::plot(id, add = TRUE)
-#' inlmisc::AddInsetMap(id, width = 2, main.label = list("IDAHO", adj = c(0, -10)),
+#' PlotMap(county, xlim = ext[1:2], ylim = ext[3:4], dms.tick = TRUE)
+#' sp::plot(county, add = TRUE)
+#' inlmisc::AddInsetMap(county, width = 2, main.label = list("IDAHO", adj = c(0, -10)),
 #'                      sub.label=list("Map area", adj = c(0, -4)), loc = "topright")
 #'
 
