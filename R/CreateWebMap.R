153185e2e770df2dfce2c7248ab59f0132810e0b
Jason Fisher <jfisher@usgs.gov>
2018-10-10 20:33:55 -0700

    Minor change

--- R/CreateWebMap.R
+++ R/CreateWebMap.R
@@ -40,7 +40,7 @@
 #' @export
 #'
 #' @examples
-#' map <- CreateWebMap()
+#' map <- CreateWebMap(collapsed = FALSE)
 #' lng <- c(-112.049705, -122.171257, -77.367458, -149.803565, -80.248344)
 #' lat <- c(43.517810, 37.456526, 38.947206, 61.187905, 26.080860)
 #' map <- leaflet::addMarkers(map, lng, lat)
