12bd3ee802d116ed09600faa1ba87487f1d7d26f
Jason Fisher <jfisher@usgs.gov>
2018-10-12 15:23:48 -0700

    Change arg name from lim to ran

--- R/SetPaletteMidpoint.R
+++ R/SetPaletteMidpoint.R
@@ -2,7 +2,7 @@
 #'
 #' Move the critical midpoint in a diverging color scheme.
 #'
-#' @param lim 'numeric' vector of length 2 or more.
+#' @param ran 'numeric' vector of length 2 or more.
 #'   Data range or values that will be used to calculate the range.
 #' @param mid 'numeric' number.
 #'   Critical midpoint of data range, defaults to 0.
@@ -25,68 +25,68 @@
 #' @export
 #'
 #' @examples
-#' Fun <- SetPaletteMidpoint(lim = c(-3, 7))
+#' Fun <- SetPaletteMidpoint(ran = c(-3, 7))
 #' inlmisc:::plot.Tol(Fun(10))
 #'
-#' # Data range (lim)
+#' # Data range (ran)
 #' n <- 10
 #' op <- par(mfrow = c(3, 1), oma = c(0, 0, 0, 0))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim = c( -5,  5))(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim = c(-10,  0))(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim = c(  0, 10))(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran = c( -5,  5))(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran = c(-10,  0))(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran = c(  0, 10))(n))
 #' par(op)
 #'
 #' # Diverging color schemes (scheme)
-#' lim <- c(-5, 5); n <- 255
+#' ran <- c(-5, 5); n <- 255
 #' op <- par(mfrow = c(3, 1), oma = c(0, 0, 0, 0))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, scheme = "sunset")(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, scheme = "BuRd")(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, scheme = "PRGn")(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, scheme = "sunset")(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, scheme = "BuRd")(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, scheme = "PRGn")(n))
 #' par(op)
 #'
 #' # Midpoint of data range (mid)
-#' lim <- c(-5, 5); n <- 255
+#' ran <- c(-5, 5); n <- 255
 #' op <- par(mfrow = c(5, 1), oma = c(0, 0, 0, 0))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, mid = -6)(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, mid = -2)(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, mid =  0)(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, mid =  2)(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, mid =  6)(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, mid = -6)(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, mid = -2)(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, mid =  0)(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, mid =  2)(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, mid =  6)(n))
 #' par(op)
 #'
 #' # Midpont buffer (buffer)
-#' lim <- c(-5, 5); n <- 10
+#' ran <- c(-5, 5); n <- 10
 #' op <- par(mfrow = c(5, 1), oma = c(0, 0, 0, 0))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, buffer = 0.0)(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, buffer = 0.1)(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, buffer = 0.2)(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, buffer = 0.3)(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, buffer = 0.4)(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, buffer = 0.0)(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, buffer = 0.1)(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, buffer = 0.2)(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, buffer = 0.3)(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, buffer = 0.4)(n))
 #' par(op)
 #'
 #' # Allow bias in color spacing (bias)
-#' lim <- c(-3, 7); n <- 20
+#' ran <- c(-3, 7); n <- 20
 #' op <- par(mfrow = c(2, 1), oma = c(0, 0, 0, 0))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, bias = TRUE)(n))
-#' inlmisc:::plot.Tol(SetPaletteMidpoint(lim, bias = FALSE)(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, bias = TRUE)(n))
+#' inlmisc:::plot.Tol(SetPaletteMidpoint(ran, bias = FALSE)(n))
 #' par(op)
 #'
 
-SetPaletteMidpoint <- function(lim, mid=0, scheme=c("sunset", "BuRd", "PRGn"),
+SetPaletteMidpoint <- function(ran, mid=0, scheme=c("sunset", "BuRd", "PRGn"),
                                buffer=0, bias=TRUE) {
 
-  checkmate::assertNumeric(lim, all.missing=FALSE, min.len=2)
+  checkmate::assertNumeric(ran, all.missing=FALSE, min.len=2)
   checkmate::assertNumber(mid, finite=TRUE)
   scheme <- match.arg(scheme)
   checkmate::qassert(buffer, "N1[0, 0.5)")
   checkmate::assertFlag(bias)
 
-  lim <- range(lim, na.rm=TRUE)
-  if (lim[1] == lim[2]) stop()
+  ran <- range(ran, na.rm=TRUE)
+  if (ran[1] == ran[2]) stop()
 
-  if (lim[1] < mid & lim[2] > mid) {
-    ratio <- diff(c(lim[1], mid)) / diff(lim)
-  } else if (lim[1] < mid) {
+  if (ran[1] < mid & ran[2] > mid) {
+    ratio <- diff(c(ran[1], mid)) / diff(ran)
+  } else if (ran[1] < mid) {
     ratio <- 1
   } else {
     ratio <- 0
@@ -95,8 +95,8 @@ SetPaletteMidpoint <- function(lim, mid=0, scheme=c("sunset", "BuRd", "PRGn"),
   if (bias || ratio %in% c(0, 1)) {
     adj <- c(0, 0)
   } else {
-    d1 <- diff(c(lim[1], mid))
-    d2 <- diff(c(mid, lim[2]))
+    d1 <- diff(c(ran[1], mid))
+    d2 <- diff(c(mid, ran[2]))
     if (d1 < d2)
       adj <- c((1 - d1 / d2) / 2, 0)
     else
