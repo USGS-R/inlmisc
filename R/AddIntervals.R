277c54ed1820c6d036f3e9f19fd91b4b07cab03c
Jason Fisher <jfisher@usgs.gov>
2019-05-06 22:50:27 -0700

    Minor changes

--- R/AddIntervals.R
+++ R/AddIntervals.R
@@ -11,16 +11,17 @@
 #' @param length 'numeric' number.
 #'   Horizontal length of the interval head, in inches.
 #' @param code 'integer' number.
-#'   Determines the type of interval to be drawn.
-#' @param col,lty,lwd,xpd graphical parameters; see \code{\link{par}}.
+#'   Determines the type of interval to be drawn, see \sQuote{Details} section for code descriptions.
+#' @param col,lty,lwd,xpd graphical parameters; see \code{\link{par}} for details.
 #'   \code{NA} values in \code{col} cause the interval to be omitted.
 #'
 #' @details For each \code{i}, an interval is drawn between the
 #'   point \code{(x[i], y0[i])} and the point \code{(x[i], y1[i])}.
+#'
 #'   Code descriptions are as follows:
 #'   \code{code = 0}, no interval head is drawn;
-#'   \code{code = 1}, an interval head is drawn at \code{(x[i], y0[i])};
-#'   \code{code = 2}, an interval head is drawn at \code{(x[i], y1[i])}; and
+#'   \code{code = 1}, a head is drawn at \code{(x[i], y0[i])};
+#'   \code{code = 2}, a head is drawn at \code{(x[i], y1[i])}; and
 #'   \code{code = 3}, a head is drawn at both ends of the interval.
 #'
 #' @return Used for the side-effect of interval symbols drawn on the current graphics device.
@@ -81,11 +82,9 @@ AddIntervals <- function(x, y0, y1, length=0.03, code=3,
 
   if (code == 0L) return(invisible(NULL))
   if (code %in% c(1L, 3L))
-    graphics::segments(m[, 1], y0, x1=m[, 2],
-                       col=col, lty=lty, lwd=lwd, xpd=xpd, lend=0)
+    graphics::segments(m[, 1], y0, x1=m[, 2], col=col, lwd=lwd, xpd=xpd, lend=0)
   if (code %in% c(2L, 3L))
-    graphics::segments(m[, 1], y1, x1=m[, 2],
-                       col=col, lty=lty, lwd=lwd, xpd=xpd, lend=0)
+    graphics::segments(m[, 1], y1, x1=m[, 2], col=col, lwd=lwd, xpd=xpd, lend=0)
 
   invisible()
 }
